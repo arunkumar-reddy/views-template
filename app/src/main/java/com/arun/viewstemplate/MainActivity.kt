457624a1b3c260a359b3633cd1aa601489da841f
arunkumar-reddy <arunkumarreddy1997@gmail.com>
2024-10-02 17:44:36 +0530

    feat: created basic views template

---
+++ app/src/main/java/com/arun/viewstemplate/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.arun.viewstemplate
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.fragment.findNavController
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.toolbar))
+        supportFragmentManager.findFragmentById(R.id.nav_host_fragment_content_main)?.let { navHostFragment ->
+            val navController = navHostFragment.findNavController()
+            appBarConfiguration = AppBarConfiguration(navController.graph)
+            setupActionBarWithNavController(navController, appBarConfiguration)
+        }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        return navController.navigateUp(appBarConfiguration)
+                || super.onSupportNavigateUp()
+    }
+}
